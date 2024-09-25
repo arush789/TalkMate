33a4d31446b97e229a4e932aae9066c49cbb7e9e
Arush <55015285+aruahaha@users.noreply.github.com>
2024-09-25 21:57:38 +0530

    socket fix

--- app/utils/APIroutes.js
+++ app/utils/APIroutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://grotesque-evvy-talkmate00-76547d50.koyeb.app/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
